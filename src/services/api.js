6a75dd2e947944bff39b88781129b7165261941f
NicoGithub758 <115185710+NicoGithub758@users.noreply.github.com>
2025-05-19 20:28:13 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://<TU_BACKEND>.railway.app", // Cambia esto
+    baseURL: "https://back-production-7af9.up.railway.app", // Cambia esto
 });
 
 export const registerUser = (data) => API.post("/auth/register", data);
