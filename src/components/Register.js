4791c77813284a0886618630ed76272958e52650
NicoGithub758 <115185710+NicoGithub758@users.noreply.github.com>
2025-05-20 20:25:44 -0300

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -20,7 +20,7 @@ const Register = () => {
 
     const handleRegister = async (e) => {
         e.preventDefault();
-        const response = await fetch("${process.env.REACT_APP_API_URL}/register", {
+        const response = await fetch("https://back-production-7af9.up.railway.app/auth/register", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(form),
