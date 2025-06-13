fceca96f8d127e969773a9289ffd0e7d1a0b6fc6
Santi53197442 <santipereyra64@gmail.com>
2025-06-13 18:55:55 -0300

    lol

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+// src/main.jsx - SOLUCIÓN CORRECTA
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+    <React.StrictMode>
+        {/* SOLUCIÓN: BrowserRouter es el proveedor principal que envuelve a todo. */}
+        <BrowserRouter>
+            {/* Ahora, AuthProvider está DENTRO del contexto del Router. */}
+            {/* Cuando se cargue y llame a useNavigate, funcionará. */}
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
