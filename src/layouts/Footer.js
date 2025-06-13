a1d64288f0aebaa2cc43bb719412315c7f3965ca
Santi53197442 <santipereyra64@gmail.com>
2025-06-13 19:41:09 -0300

    lindo

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -24,9 +24,7 @@ const Footer = () => {
                     <ul>
                         {/* Reemplazamos todos los <a> internos por <Link> */}
                         <li><Link to="/destinos"><i className="fas fa-route"></i> Destinos</Link></li>
-                        <li><Link to="/trabaja-con-nosotros"><i className="fas fa-briefcase"></i> Trabajá con nosotros</Link></li>
                         <li><Link to="/contacto"><i className="fas fa-envelope"></i> Contacto</Link></li>
-                        <li><Link to="/colaboradores"><i className="fas fa-users"></i> Colaboradores</Link></li>
 
                         {/* 'mailto:' es un enlace externo, así que mantenemos <a> */}
                         <li><a href="mailto:info@carpibus.com.uy"><i className="fas fa-at"></i> info@carpibus.com.uy</a></li>
@@ -42,8 +40,11 @@ const Footer = () => {
                         <i className="fab fa-cc-amex" title="American Express"></i>
                     </div>
                     <div className="social-icons">
-                        {/* Estos deberían apuntar a tus perfiles sociales reales */}
-                        <a href="https://www.google.com/maps" target="_blank" rel="noopener noreferrer" aria-label="Nuestra ubicación"><i className="fas fa-map-marked-alt"></i></a>
+
+                        {/* --- ESTA ES LA LÍNEA MODIFICADA --- */}
+                        <a href="https://maps.app.goo.gl/ptJgk7WsJu5k4c8D7" target="_blank" rel="noopener noreferrer" aria-label="Nuestra ubicación"><i className="fas fa-map-marked-alt"></i></a>
+
+                        {/* El resto de los enlaces sociales */}
                         <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><i className="fab fa-facebook-f"></i></a>
                         <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><i className="fab fa-instagram"></i></a>
                         <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" aria-label="YouTube"><i className="fab fa-youtube"></i></a>
